d4356ae159370cf238795d5e7f62b5cccd139608
saaemfairdi07 <officialsaaem@gmail.com>
2025-10-16 13:49:42 +0530

    Vercel Analytics and Speed Insights setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import type { Metadata, Viewport } from 'next';
 import { Inter } from 'next/font/google';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ 
   subsets: ['latin'],
@@ -60,6 +62,9 @@ export default function RootLayout({
         <Header />
         <main className="min-h-screen">{children}</main>
         <Footer />
+
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
